7b999116b67a1b6b7d2c22af69f4b37999c2bace
indu0710 <108384463+indu0710@users.noreply.github.com>
2022-07-11 20:00:20 -0700

    Update wageReportAPI.py

--- wageReportAPI.py
+++ wageReportAPI.py
@@ -2,10 +2,13 @@
 
 #Last updated
 # 6/28/2022 by F. Peterson first file creation 30 lines of code
+# 7/11/2022 by I. Kolli API code fixed
 
-
+# from requests import request as req
+from requests import request as rq
 from flask import Flask, request, send_file
-from requests import request #https://pypi.org/project/requests/
+import requests #https://pypi.org/project/requests/
+# from . import app
 
 from wagetheft_report_2020_v2 import generateWageReport
 #generateWageReport from wagetheft_report_2020_v2
@@ -20,23 +23,20 @@ def generateWageReport(INPUT_PARAM):
 app = Flask(__name__)
    
 @app.route('/generateWageReport', methods=['POST'])
-
 def generateFile():
     parameters = request.get_json()
     param_1_Ziprcode = parameters["param1"] #user input from website field_1
-    param_2_Industry = parameters["param1"] #user input from website field_2
+    #param_2_Industry = parameters["param1"] #user input from website field_2
     #...
     
-    wageReportFileName = generateWageReport(param_1_Ziprcode, param_2_Industry) #returns PDF file
+    #wageReportFileName = generateWageReport(param_1_Ziprcode, param_2_Industry) #returns PDF file
     
-    try:
-        file_path = "./"  +  wageReportFileName
-        return  send_file(file_path, 'fileName')
+    #try:
+        #file_path = "./"  +  wageReportFileName
+        #return  send_file(file_path, 'fileName')
     
-    except Exception as e:
-        return  str(e)
-
-generateFile()
+    #except Exception as e:
+        #return  str(e)
 
 
 
