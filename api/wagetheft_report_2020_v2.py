42a910d4e4b135bccd3e975e517e79bf192e11c8
aosegpadg <aoseguera@paloaltodatagroup.com>
2022-08-11 19:07:36 -0700

    Completed supressing all warnings

--- api/wagetheft_report_2020_v2.py
+++ api/wagetheft_report_2020_v2.py
@@ -1030,10 +1030,9 @@ def Clean_Repeat_Violator_HTML_Row(df, COLUMN_NAME):
     df = df[df.records > 1]
 
     if df.empty:
-        df = df.append({
+        df = pd.concat([df, {
             COLUMN_NAME: "no records",
-        }, ignore_index=True)
-
+        }], ignore_index=True)
     return df
 
 
