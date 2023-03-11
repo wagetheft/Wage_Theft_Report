a7daee0f6318cfe5f7f2f3ac458b001be5a75da9
Forest <fpeterson@paloaltodatagroup.com>
2023-03-11 15:50:13 -0800

    updated WHD data url

--- api/wagetheft_report_2020_v3.py
+++ api/wagetheft_report_2020_v3.py
@@ -241,7 +241,7 @@ def generateWageReport(target_state, target_county, target_city, target_industry
     time_1 = time.time()
 
     url0 = "https://stanford.edu/~granite/DLSE_no_returns_Linux_TEST.csv" #<-- open and edit this file with test data
-    url1 = "https://enfxfr.dol.gov/data_catalog/WHD/whd_whisard_20221006.csv.zip"
+    url1 = "https://enfxfr.dol.gov/data_catalog/WHD/whd_whisard_20230226.csv.zip" #update link from here https://enforcedata.dol.gov/views/data_catalogs.php
     # url2 = "https://www.researchgate.net/profile/Forest-Peterson/publication/357767172_California_Dept_of_Labor_Standards_Enforcement_DLSE_PRA_Wage_Claim_Adjudications_WCA_for_all_DLSE_offices_from_January_2001_to_July_2019/data/61de6b974e4aff4a643603ae/HQ20009-HQ-2nd-Production-8132019.csv"
     # url2 = "https://drive.google.com/file/d/1TRaixcwTg08bEyPSchyHntkkktG2cuc-/view?usp=sharing"
     url2 = "https://stanford.edu/~granite/HQ20009-HQ2ndProduction8.13.2019_no_returns_Linux.csv" #10/2/2022 added _Linux
