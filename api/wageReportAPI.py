1a25b878282418eb4aef419102a7fbddcc7d4444
Forest <fpeterson@paloaltodatagroup.com>
2024-01-15 12:39:39 -0800

    updated with wage case checkbox

--- api/wageReportAPI.py
+++ api/wageReportAPI.py
@@ -23,20 +23,22 @@ def generateFile():
     target_city = parameters["target_city"] if "target_city" in parameters else ""
     target_industry = parameters["target_industry"]
     target_organization = parameters["target_organization"]
+    
     includeFedData = parameters["includeFedData"]
-    includeStateData = parameters["includeStateData"]
+    includeStateJudgements = parameters["includeStateJudgements"]
+    includeStateCases = parameters["includeStateCases"]
     infer_zip = parameters["infer_zip"]
+    use_assumptions = parameters["use_assumptions"]
+    infer_by_naics = parameters["infer_by_naics"]
+    open_cases_only = parameters["open_cases_only"]
+
     prevailing_wage_report = parameters["prevailing_wage_report"]
     signatories_report = parameters["signatories_report"]
-    all_industry_summary_block = parameters["all_industry_summary_block"] # delete option
-    open_cases_only = parameters["open_cases_only"]
     include_tables = parameters["include_tables"]
     include_summaries = parameters["include_summaries"]
     only_sig_summaries = parameters["only_sig_summaries"]
     include_top_viol_tables = parameters["include_top_viol_tables"]
-    use_assumptions = parameters["use_assumptions"]
-    infer_by_naics = parameters["infer_by_naics"]
-
+    
     YEAR_START = parameters["YEAR_START"] if "YEAR_START" in parameters else ""
     YEAR_END = parameters["YEAR_END"] if "YEAR_END" in parameters else ""
 
@@ -61,8 +63,10 @@ def generateFile():
 
 
     #out it goes
-    report_file_name = generateWageReport(target_state, target_county, target_city, target_industry, target_organization, includeFedData, includeStateData, infer_zip, prevailing_wage_report, signatories_report,
-                                          open_cases_only, include_tables, include_summaries, only_sig_summaries, include_top_viol_tables, use_assumptions, infer_by_naics, YEAR_START, YEAR_END)
+    report_file_name = generateWageReport(target_state, target_county, target_city, target_industry, target_organization, includeFedData, \
+                                          includeStateJudgements, includeStateCases, infer_zip, prevailing_wage_report, signatories_report, \
+                                            open_cases_only, include_tables, include_summaries, only_sig_summaries, \
+                                            include_top_viol_tables, use_assumptions, infer_by_naics, YEAR_START, YEAR_END)
 
     try:
         return send_file(report_file_name, as_attachment=True)
@@ -71,7 +75,13 @@ def generateFile():
 
 
 def checkValidInput(inputDict) -> bool:
-    if "target_city" not in inputDict or "target_industry" not in inputDict or "target_organization" not in inputDict or "includeFedData" not in inputDict or "includeStateData" not in inputDict or "infer_zip" not in inputDict or "prevailing_wage_report" not in inputDict or "signatories_report" not in inputDict or "open_cases_only" not in inputDict or "include_tables" not in inputDict or "include_summaries" not in inputDict or "only_sig_summaries" not in inputDict or "include_top_viol_tables" not in inputDict or "use_assumptions" not in inputDict or "infer_by_naics" not in inputDict or "YEAR_START"  not in inputDict or "YEAR_END" not in inputDict :
+    if "target_city" not in inputDict or "target_industry" not in inputDict or "target_organization" not in inputDict or \
+        "includeFedData" not in inputDict or "includeStateJudgements" not in inputDict or "includeStateCases" not in inputDict\
+            or "infer_zip" not in inputDict or \
+            "prevailing_wage_report" not in inputDict or "signatories_report" not in inputDict or "open_cases_only" not in inputDict \
+                or "include_tables" not in inputDict or "include_summaries" not in inputDict or "only_sig_summaries" not in inputDict or \
+                    "include_top_viol_tables" not in inputDict or "use_assumptions" not in inputDict or \
+                        "infer_by_naics" not in inputDict or "YEAR_START"  not in inputDict or "YEAR_END" not in inputDict :
         return False
 
     return True
