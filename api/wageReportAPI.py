331622c1514aa2669f7eddf053555075d072f65c
Forest <fpeterson@paloaltodatagroup.com>
2024-04-25 13:42:25 -0700

    added logfile of call

--- api/wageReportAPI.py
+++ api/wageReportAPI.py
@@ -22,7 +22,7 @@ def generateFile():
     target_county = parameters["target_county"] if "target_county" in parameters else ""
     target_city = parameters["target_city"] if "target_city" in parameters else ""
 
-    target_industry = parameters["target_industry"] if "target_industry" in parameters else "ALL NAICS"
+    target_industry = parameters["target_industry"] if "target_industry" in parameters else ""
 
     target_organization = parameters["target_organization"] if "target_organization" in parameters else ""
     
@@ -72,9 +72,16 @@ def generateFile():
         infer_zip = 1
         infer_by_naics = 1
 
+    #log
+    logfile(target_state, target_county, target_city, target_industry, \
+                target_organization, \
+                includeFedData, includeStateJudgements, includeStateCases, infer_zip, prevailing_wage_report, signatories_report, \
+                open_cases_only, include_tables, include_summaries, only_sig_summaries, \
+                include_top_viol_tables, use_assumptions, infer_by_naics, YEAR_START_TEXT, YEAR_END_TEXT)
 
     #out it goes
-    report_file_name = generateWageReport(target_state, target_county, target_city, target_industry, target_organization, \
+    report_file_name = generateWageReport(target_state, target_county, target_city, target_industry, \
+                                          target_organization, \
                                           includeFedData, includeStateJudgements, includeStateCases, infer_zip, prevailing_wage_report, signatories_report, \
                                             open_cases_only, include_tables, include_summaries, only_sig_summaries, \
                                             include_top_viol_tables, use_assumptions, infer_by_naics, YEAR_START_TEXT, YEAR_END_TEXT)
@@ -85,9 +92,90 @@ def generateFile():
         return "Server error", 500
 
 def checkValidInput(inputDict: dict) -> bool:
-    toCheck = ["target_city", "target_industry", "target_organization", 
+    toCheck = ["target_city", "target_industry", 
+               "target_organization", 
                "includeFedData", "includeStateJudgements", "includeStateCases", "infer_zip", "prevailing_wage_report", "signatories_report", 
                "open_cases_only", "include_tables", "include_summaries", "only_sig_summaries", 
                "include_top_viol_tables",  "use_assumptions",  "infer_by_naics", "YEAR_START", "YEAR_END"]
     return all(key in inputDict for key in toCheck)
+
+def logfile(target_state, target_county, target_city, target_industry, \
+                target_organization, \
+                includeFedData, includeStateJudgements, includeStateCases, infer_zip, prevailing_wage_report, signatories_report, \
+                open_cases_only, include_tables, include_summaries, only_sig_summaries, \
+                include_top_viol_tables, use_assumptions, infer_by_naics, YEAR_START_TEXT, YEAR_END_TEXT):
+    import os
+    
+    rel_path = 'api_logs_/'
+
+    # <-- dir the script is in (import os) plus up one
+    script_dir = os.path.dirname(os.path.dirname(__file__))
+    abs_path = os.path.join(script_dir, rel_path)
+    os.chdir(script_dir)
+
+    if not os.path.exists(script_dir):  # create folder if necessary
+        os.makedirs(script_dir)
+
+    if not os.path.exists(abs_path):  # create folder if necessary
+        os.makedirs(abs_path)
+
+    file_name = 'api_query'
+    file_type = '.txt'
+
+    api_log_file = os.path.join(abs_path, (file_name).replace(
+        ' ', '_') + file_type)  # <-- absolute dir and file name
+
+    bugFile = open(api_log_file, 'a')
+
+    bugFile.write("<!DOCTYPE html>")
+    bugFile.write("\n")
+    bugFile.write("<html><body>")
+    bugFile.write("\n")
+    bugFile.write("<h1>API CALL</h1>")
+    bugFile.write("\n")
+    bugFile.write("\n target_state \n")
+    bugFile.write(target_state)
+    bugFile.write("\n target_county \n")
+    bugFile.write(target_county)
+    bugFile.write("\n target_city \n")
+    bugFile.write(target_city)
+    bugFile.write("\n target_industry \n")
+    bugFile.write(target_industry)
+    bugFile.write("\n target_organization \n")
+    bugFile.write(target_organization)
+    bugFile.write("\n includeFedData \n")
+    bugFile.write(str(includeFedData) )
+    bugFile.write("\n includeStateJudgements \n")
+    bugFile.write(str(includeStateJudgements))
+    bugFile.write("\n includeStateCases \n")
+    bugFile.write(str(includeStateCases))
+    bugFile.write("\n infer_zip \n")
+    bugFile.write(str(infer_zip))
+    bugFile.write("\n prevailing_wage_report \n")
+    bugFile.write(str(prevailing_wage_report))
+    bugFile.write("\n signatories_report \n")
+    bugFile.write(str(signatories_report))
+    bugFile.write("\n open_cases_only \n")
+    bugFile.write(str(open_cases_only))
+    bugFile.write("\n include_tables \n")
+    bugFile.write(str(include_tables))
+    bugFile.write("\n include_summaries \n")
+    bugFile.write(str(include_summaries))
+    bugFile.write("\n only_sig_summaries \n")
+    bugFile.write(str(only_sig_summaries))
+    bugFile.write("\n include_top_viol_tables \n")
+    bugFile.write(str(include_top_viol_tables))
+    bugFile.write("\n use_assumptions \n")
+    bugFile.write(str(use_assumptions)) 
+    bugFile.write("\n infer_by_naics \n")
+    bugFile.write(str(infer_by_naics))
+    bugFile.write("\n")
+    bugFile.write(YEAR_START_TEXT)
+    bugFile.write("\n")
+    bugFile.write(YEAR_END_TEXT)
+    bugFile.write("\n")
+    bugFile.write("<h1>DONE</h1> \n" + "</html></body> \n")
+    bugFile.close()
+
+    return
 
