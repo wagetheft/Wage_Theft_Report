adb086ccf419240f92d80c37f49919e69c362077
Forest <fpeterson@paloaltodatagroup.com>
2024-06-03 18:35:34 -0700

    fix mode='a'

--- api/wagetheft_report_2020_v3.py
+++ api/wagetheft_report_2020_v3.py
@@ -1334,14 +1334,14 @@ def print_top_viol_tables_html(df, unique_address, unique_legalname2,
             </html>
             '''
 
-        with open(temp_file_name, 'a', encoding='utf-8') as f:  # append to report main file
+        with open(temp_file_name, mode='a', encoding='utf-8') as f:  # append to report main file
                 f.write(result)
     else:
         result += '''
             </body>
             </html>
             '''
-        with open(temp_file_name, 'a', encoding='utf-8') as f:  # append to report main file
+        with open(temp_file_name, mode='a', encoding='utf-8') as f:  # append to report main file
                 f.write(result)
 
 
