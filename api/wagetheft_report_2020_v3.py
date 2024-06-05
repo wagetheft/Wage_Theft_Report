e6746497f5a23da8e76f85f0ce2b6e871ec9374b
Forest <fpeterson@paloaltodatagroup.com>
2024-06-05 10:28:29 -0700

    hack fix bug condition of over selection of location

--- api/wagetheft_report_2020_v3.py
+++ api/wagetheft_report_2020_v3.py
@@ -143,6 +143,7 @@ def generateWageReport(target_state, target_county, target_city, target_industry
     include_methods = True
     if target_industry == "": target_industry = "All NAICS"
     if (target_state == "") and (target_county == "") and (target_city == ""): target_state = "California"
+    if (target_state != "") and ((target_county != "") or (target_city != "")): target_state = "" #temp fix
     
     if YEAR_START_TEXT == "":
         YEAR_START = pd.to_datetime('today') - pd.DateOffset(years=4)
