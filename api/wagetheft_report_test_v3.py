539540304dc0bc2fb7fd8c64b80c360300d6735d
Forest <fpeterson@paloaltodatagroup.com>
2025-03-29 00:16:54 -0700

    change to txt file

--- api/wagetheft_report_test_v3.py
+++ api/wagetheft_report_test_v3.py
@@ -6,17 +6,12 @@ warnings.filterwarnings("ignore", 'This pattern has match groups')
 def generateWageReport(target_state, target_county, target_city, target_industry, target_organization):
 
     warnings.filterwarnings("ignore", category=UserWarning)
-    
-    pdf_writer = PdfWriter()
-    page = pdf_writer.get_page(0)
 
-    annotation = page.new_annotation("hello world: " + target_state + " " + target_county + " " + 
+    annotation = ("hello world: " + target_state + " " + target_county + " " + 
             target_city + " " + target_industry + " " + target_organization + " !" )
 
 
-    page.annotations.append(annotation)
+    with open("output.txt", "w") as file:
+        file.write(annotation)
 
-    with open("output.pdf", "wb") as output_pdf:
-        pdf_writer.write(annotation)
-
-    return output_pdf
+    return file
