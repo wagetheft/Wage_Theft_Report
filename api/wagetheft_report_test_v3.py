9574a2a15f1cb56722885c669f2af52799ac97dc
Forest <fpeterson@paloaltodatagroup.com>
2025-03-31 00:26:55 -0700

    try simple name

--- api/wagetheft_report_test_v3.py
+++ api/wagetheft_report_test_v3.py
@@ -11,13 +11,15 @@ def generateWageReport(target_state, target_county, target_city, target_industry
     annotation = ("hello world: " + target_state + " " + target_county + " " + 
             target_city + " " + target_industry + " " + target_organization + " !" )
 
-    rel_path = 'report_output_/'
-    script_dir = os.path.dirname(os.path.dirname(__file__))
-    abs_path = os.path.join(script_dir, rel_path)
+    #rel_path = 'report_output_/'
+    #script_dir = os.path.dirname(os.path.dirname(__file__))
+    #abs_path = os.path.join(script_dir, rel_path)
 
-    file_type = ".txt"
-    temp_file_name_PDF = os.path.join(abs_path, ("test report").replace(
-        ' ', '_') + file_type)  # <-- absolute dir and file name
+    #file_type = ".txt"
+    #temp_file_name_PDF = os.path.join(abs_path, ("test report").replace(
+    #    ' ', '_') + file_type)  # <-- absolute dir and file name
+
+    temp_file_name_PDF = "test report.txt"
 
     with open(temp_file_name_PDF, "w") as file:
         file.write(annotation)
