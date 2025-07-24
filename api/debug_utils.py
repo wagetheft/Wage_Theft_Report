ac7505f8d91bb28cbfbacc970bca6f18ca354bd1
Forest <fpeterson@paloaltodatagroup.com>
2025-07-24 16:22:46 -0700

    test debug

--- api/debug_utils.py
+++ api/debug_utils.py
@@ -12,6 +12,8 @@ def debug_fileSetup_def(bug_log, LOGBUG = True):
 
         bugFile.write("<h1>START</h1>")
         bugFile.write("\n")
+        bugFile.write("TEST | TEST | TEST")
+        bugFile.write("\n")
         bugFile.close()
 
 
